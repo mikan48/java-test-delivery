96ca20d6314fdda9631cb6741ce1642b3c225e1e
Anna Latushkina <mukanshin.san@gmail.com>
2026-01-16 17:41:55 +0300

    initial spring project

---
+++ test-delivery/src/main/java/com/example/test_delivery/TestDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDeliveryApplication.class, args);
+	}
+
+}
---
+++ test-delivery/src/test/java/com/example/test_delivery/TestDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
